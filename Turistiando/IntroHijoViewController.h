94020ada6982b49414444d91fbe3f8bd88471c85
juansmacias <juansmacias@gmail.com>
2013-09-15 16:04:03 -0500

    intro

---
+++ Turistiando/IntroHijoViewController.h
@@ -0,0 +1,15 @@
+//
+//  IntroHijoViewController.h
+//  Turistiando
+//
+//  Created by Macias on 15/09/13.
+//  Copyright (c) 2013 Turistiando Colombia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface IntroHijoViewController : ViewController
+
+@property (assign,nonatomic) NSInteger *indice;
+
+@end

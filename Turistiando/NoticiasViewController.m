d68939e242064c892c86fc5390255c4ea27aaabd
juansmacias <juansmacias@gmail.com>
2013-11-21 07:38:25 -0500

    WEBSERVOCES

--- Turistiando/NoticiasViewController.m
+++ Turistiando/NoticiasViewController.m
@@ -22,13 +22,31 @@
 {
     [super viewDidLoad];
     
-
-
-    
     self.buttonBarMenu.target = self.revealViewController;
     self.buttonBarMenu.action = @selector(revealToggle:);
     
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    
+    //Prepare to establish the connection
+    NSURL *url = [NSURL URLWithString:@"http://157.253.238.144:8090/tur/webresources/webservice.usuario/find1"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+    [request setHTTPMethod:@"GET"];
+    [request setValue:@"1" forHTTPHeaderField:@"id"];
+    
+    NSLog(@"Url de envio %@",request.URL.fragment);
+    NSHTTPURLResponse *response = nil;
+    NSError *error = nil;
+    //Make the request
+    NSData* responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if(error ==nil)
+    {
+
+        
+        //Parse the data as a series of Note objects
+        NSDictionary* json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
+    }
+
     Turistiando *tour = [Turistiando darInstancia];    
     self.nombre.text = tour.nombre;
     self.nacio.text = tour.nacio;
